5c42c93b444d53183faf17a337442ca02534ec0f
Aryan Bansod <aryanem09@gmail.com>
2023-04-23 18:40:52 +0530

    connected to mongo

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose")
 const articleRouter = require('./routes/articles')
 const app = express();
+
+mongoose.connect('mongodb://localhost/blog',{
+    useNewUrlParser :true, useUnifiedTopology: true
+})
+
+
 app.set('view engine', 'ejs');
 //using articleRouter form articles
 app.use('/articles',articleRouter);
--- routes/articles.js
+++ routes/articles.js
@@ -6,5 +6,7 @@ const app = express();
 router.get('/new',(req, res)=>{
     res.render('articles/new')
 })
-
+router.post('/', (req, res )=>{
+    
+})
 module.exports = router
