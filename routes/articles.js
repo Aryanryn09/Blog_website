2d41c18a1d33a2e5cd1e7e00a96e4b00c3478e4c
Aryan Bansod <aryanem09@gmail.com>
2023-04-30 20:20:13 +0530

    updated

--- routes/articles.js
+++ routes/articles.js
@@ -6,12 +6,22 @@ const app = express();
 // app.set('view engine', 'ejs');
 
 router.get('/new', (req, res)=>{
-    res.render('articles/new')
+    res.render('articles/new', {article : new Article()})
 })
 
+router.post('/', (req, res)=>{
+    
+})
 
-router.get('./:id', (req, res)=>{
-
+router.get('./:id', async (req, res)=>{
+    // rome-ignore lint/style/useConst: <explanation>
+const  article = await Article.findByI (req.params.id);
+if(article == null){
+    res.render('articles/show', {article : article } )
+}
+})
+router.get('/new', (req, res)=>{
+  res.render('articles/new', { article : new Article() })  
 })
 
 
